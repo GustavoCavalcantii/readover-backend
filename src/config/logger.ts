fa92749a9b4fd3e86cca1f7741419412aaeaa914
GustavoCavalcantii <gustavocavalcanti096@gmail.com>
2025-03-16 02:06:03 -0300

    feat: adicionar logger

---
+++ src/config/logger.ts
@@ -0,0 +1,67 @@
+import winston from "winston";
+import path from "path";
+import fs from "fs";
+
+const logsDirectory = path.join(__dirname, "../../logs");
+if (!fs.existsSync(logsDirectory)) {
+  fs.mkdirSync(logsDirectory);
+}
+
+const logPath = path.join(logsDirectory, "app.log");
+
+const addEmote = (level: string): string => {
+  switch (level) {
+    case "info":
+      return "❕";
+    case "warn":
+      return "⚠️";
+    case "error":
+      return "❌";
+    case "debug":
+      return "🔍";
+    default:
+      return "📝";
+  }
+};
+
+const logger = winston.createLogger({
+  level: "debug",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      const emote = addEmote(level);
+      const coloredLevel = winston.format.colorize().colorize(level, level);
+      return `${timestamp} ${emote} ${coloredLevel} : ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+          const emote = addEmote(level);
+          let space = "";
+          if (level === "warn") {
+            space = "  "; 
+          } else {
+            space = " "; 
+          }
+          const coloredLevel = winston.format.colorize().colorize(level, level);
+          return `${timestamp} ${emote}${space}${coloredLevel} : ${message}`;
+        })
+      ),
+    }),
+    new winston.transports.File({
+      filename: logPath,
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+          const emote = addEmote(level);
+          return `${timestamp} ${emote} ${level} : ${message}`;
+        })
+      ),
+    }),
+  ],
+});
+
+export default logger;
