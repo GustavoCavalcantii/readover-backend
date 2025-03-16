b4cca970e1f6a8ff8779d574dd953db244e42bc7
GustavoCavalcantii <gustavocavalcanti096@gmail.com>
2025-03-16 16:14:14 -0300

    refactor: mudando as informações no console

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,20 +1,20 @@
 import logger from "../config/logger";
 import { ApiEnviroment } from "../enum/Api/ApiEnviroment";
 import app from "../routes/app";
-import {generateASCII} from "../utils/nameGenerator"
-
+import { generateASCII } from "../utils/nameGenerator";
 import { connectDB, disconnectDB } from "../database/connection";
-
 import { config } from "dotenv";
+import packageJson from "../../package.json";
+
 config();
 
-const API_NAME: string = process.env.APP_NAME as string;
-const API_VERSION: string = process.env.API_VERSION as string;
-const ENVIROMENT: string = process.env.ENVIROMENT === "dev" ? ApiEnviroment.DEV : ApiEnviroment.PROD;
+const API_NAME: string = packageJson.name;
+const APP_VERSION: string = packageJson.version;
+const API_VERSION: string = "v" + (process.env.API_VERSION || "1"); 
+const ENVIRONMENT: string = process.env.ENVIRONMENT === "dev" ? ApiEnviroment.DEV : ApiEnviroment.PROD; 
 const PORT: number = Number(process.env.PORT) || 8088;
 
-const stopServer = async () => 
-{
+const stopServer = async () => {
   logger.warn("Parando a aplicação...");
   await disconnectDB();
   logger.warn("Conexão com o banco de dados encerrada");
@@ -23,10 +23,11 @@ const stopServer = async () =>
   setTimeout(() => {
     process.exit(0);
   }, 1000);
-}
+};
 
 function getHorizontalSize(asciiArt: string): number {
-  return Math.max(...asciiArt.split("\n").map((line) => line.length));
+  const size = Math.max(...asciiArt.split("\n").map((line) => line.length));
+  return size;
 }
 
 const startServer = async () => {
@@ -35,16 +36,17 @@ const startServer = async () => {
 
     console.log("Conectando com o banco de dados...");
     await connectDB();
-    const horizontaSize = getHorizontalSize(asciiArt) + 1;
+    const horizontalSize = getHorizontalSize(asciiArt) + 1;
 
     console.log(asciiArt);
-    console.log("=".repeat(horizontaSize));
-    console.log(`Versão: ${API_VERSION}`);
-    console.log(`Ambiente: ${ENVIROMENT}`);
-    console.log("=".repeat(horizontaSize));
+    console.log("=".repeat(horizontalSize));
+    console.log(`Versão do app: ${APP_VERSION}`);
+    console.log(`Versão API: ${API_VERSION}`);
+    console.log(`Ambiente: ${ENVIRONMENT}`);
+    console.log("=".repeat(horizontalSize));
 
     app.listen(PORT, () => {
-      logger.info(`Aplicação inciada com sucesso!`);
+      logger.info(`Aplicação iniciada com sucesso!`);
     });
   } catch (err) {
     logger.error(`Erro ao iniciar aplicação: ${err}`);
