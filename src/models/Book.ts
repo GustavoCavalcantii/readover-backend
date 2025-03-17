6962287e8e20e7a825544de106beb1b3e40cb77e
Thi4goSer4fim <serafimthiago052@gmail.com>
2025-03-16 22:13:48 -0300

    feat: adicionar Interface, Model e Service para livros

---
+++ src/models/Book.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from "mongoose";
+import { IBook } from "../interface/IBook";
+
+const bookSchema = new Schema<IBook>({
+    title: {type: String, required: true},
+    author: {type: String, required: true},
+    isbn: {type: String, required: true, unique: true},
+    quantityAvailable: {type: Number, default: 1},
+    description: {type: String},
+    category: {type: String, required: true},
+    linkPdf: {type: String, unique: true},
+});
+
+const Book = mongoose.model<IBook>("Book", bookSchema);
+
+export default Book;
---
+++ src/service/bookService.ts
@@ -0,0 +1,55 @@
+import { IBook } from "../interface/IBook";
+import Book from "../models/Book";
+import { MongoServerError } from "mongodb";
+
+export class BookService {
+  async createBook(
+    title: string,
+    author: string,
+    isbn: string,
+    category: string
+  ): Promise<IBook> {
+    const book = new Book({
+      title,
+      author,
+      isbn,
+      category
+    });
+
+    try {
+      await book.save();
+      return book;
+    } catch (error) {
+      if (error instanceof MongoServerError && error.code === 11000) {
+        throw new Error("Este isbn já está cadastrado. Tente outro.");
+      }
+
+      throw new Error("Ocorreu um erro desconhecido.");
+    }
+  }
+
+  async getBookByIsbn(isbn: string): Promise<IBook | null> {
+    const book = await Book.findOne({ isbn });
+    return book;
+  }
+
+  async getBookByTitle(title: string): Promise<IBook | null> {
+    const book = await Book.findOne({ title });
+    return book;
+  }
+
+  async getBookByAuthor(author: string): Promise<IBook | null> {
+    const book = await Book.findOne({ author });
+    return book;
+  }
+
+  async getBookByCategory(category: string): Promise<IBook | null> {
+    const book = await Book.findOne({ category });
+    return book;
+  }
+
+  async getBookById(id: string): Promise<IBook | null> {
+    const book = await Book.findById(id);
+    return book;
+  }
+}
