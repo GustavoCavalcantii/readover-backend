6b1015f8320926f888da5615f4dc50f3afcdf3b9
GustavoCavalcantii <gustavocavalcanti096@gmail.com>
2025-05-05 17:43:00 -0300

    refactor: remover limiter

--- src/routes.ts
+++ src/routes.ts
@@ -25,7 +25,7 @@ const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
 /*
   MIDDLEWARES
 */
-app.use(limiter);
+//app.use(limiter);
 app.use(cookieParser());
 app.set("trust proxy", true);
 app.use(express.json());
